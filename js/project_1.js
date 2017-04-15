030e1f236dacfc093895f0411e64cf1cddd30a13
Joe Burns <sephburns1@gmail.com>
2017-04-15 16:03:23 +0100

    Test

---
+++ js/project_1.js
@@ -0,0 +1,39 @@
+//this worked but it only detected if scrolling, not when scrolling had stopped
+
+//window.onscroll = function() {changeBg()};
+
+//function changeBg() {
+
+  //document.getElementById("character").style.backgroundColor = "red";
+
+//}
+
+//end of script
+
+var scrolling = false;
+
+window.scroll(function()) {
+
+  scrolling == true;
+
+  Console.log(scrolling);
+
+}
+
+
+
+
+
+//The following is for detecting how far you have scrolled:
+
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onscroll2
+
+//window.onscroll = function() {characterProgress()};
+
+//function characterProgress() {
+//    if (document.body.scrollleft > 50 || document.documentElement.scrollleft > 50) {
+//        document.getElementById("character").style.backgroundColor = "pink";
+//    } 
+}
+
+  
